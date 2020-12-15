d92608357fcda757ff0712097c4be2cfa2f54418
Evgeniy <jacksonrr3@yandex.ru>
2020-12-15 22:06:06 +0300

    data

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, abort
 
-from .data import title, subtitle, description, departures, tours
+from data import title, subtitle, description, departures, tours
 
 
 app = Flask(__name__)
